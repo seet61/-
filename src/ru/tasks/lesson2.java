c32989e5377fc84418980aa2a61838eb1120676e
seet61 <seet_seet@mail.ru>
2018-05-10 18:46:32 +0300

    lesson2

---
+++ src/ru/tasks/lesson2.java
@@ -0,0 +1,18 @@
+package ru.tasks;
+
+public class lesson2 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Double.POSITIVE_INFINITY);
+        System.out.println(Double.NEGATIVE_INFINITY);
+
+        System.out.println((short) Integer.MAX_VALUE);
+        System.out.println((short) Integer.MIN_VALUE);
+
+        System.out.println(Float.POSITIVE_INFINITY);
+        System.out.println(Float.NEGATIVE_INFINITY);
+
+        System.out.println((long) Float.POSITIVE_INFINITY);
+    }
+}
