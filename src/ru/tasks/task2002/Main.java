d2a41301d46874ff536de0b1579c4c7997a6ba10
seet61 <seet_seet@mail.ru>
2018-05-10 10:55:29 +0300

    completed 2002 task

--- src/ru/tasks/task2002/Main.java
+++ src/ru/tasks/task2002/Main.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Введите размерность массива чисел: ");
+
         Scanner scanner = new Scanner(System.in);
-        String pattern = "(\d{1,5}){2,}";
         int count = scanner.nextInt();
+
+        String[] sl = scanner.nextLine().split(" ");
         int sum = 0;
-        System.out.println("Вводите числа массива: ");
-        for (int i = 0; i < count; i++) {
-            sum += scanner. nextInt();
+
+        while (!(sl.length >= count)) {
+            System.out.println("Вводите числа массива: ");
+            sl = scanner.nextLine().split(" ");
+        }
+
+        for (int i = 0; i < sl.length; i++) {
+            sum += Integer.parseInt(sl[i].toString());
         }
         System.out.println("Результат: " + sum);
+
     }
 }
