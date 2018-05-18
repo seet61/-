f3a64bf352a10f369e5489680cc68982d1d5b505
seet61 <seet_seet@mail.ru>
2018-05-18 17:46:34 +0300

    change tasks 2004-2010 with format "what"

--- src/ru/tasks/task2008/Solution.java
+++ src/ru/tasks/task2008/Solution.java
@@ -1,26 +1,38 @@
 package ru.tasks.task2008;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        int count = Integer.parseInt(st.nextToken());
-        int weight = Integer.parseInt(st.nextToken());
-        int total = 0;
-        int sum = 0;
-        st = new StringTokenizer(in.readLine());
-        for (int i = 0; i < count; i++) {
-            int cur = Integer.parseInt(st.nextToken());
-            if ((total + cur) < weight) {
-                total += cur;
-                sum += 1;
+    public static void main(String[] args) {
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int count = Integer.parseInt(st.nextToken());
+            if (count < 1 || count > 10001) {
+                throw new Exception("bad count");
             }
+            int weight = Integer.parseInt(st.nextToken());
+            if (weight < 1|| weight > 10001) {
+                throw new Exception("bad weight");
+            }
+            int total = 0;
+            int sum = 0;
+            st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < count; i++) {
+                int cur = Integer.parseInt(st.nextToken());
+                if (cur < 1 || cur > 10000) {
+                    throw new Exception("bad cur");
+                }
+                if ((total + cur) < weight) {
+                    total += cur;
+                    sum += 1;
+                }
+            }
+            System.out.print(sum + " " + total);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println(sum + " " + total);
     }
 }
--- src/ru/tasks/task2021/Solution.java
+++ src/ru/tasks/task2021/Solution.java
@@ -1,7 +1,6 @@
 package ru.tasks.task2021;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,10 +8,13 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
         int count = Integer.parseInt(st.nextToken());
+        if (count < 1 || count > 100) {
+            throw new Exception("bad count");
+        }
         List list = Arrays.asList(in.readLine().split(" "));
         for (int i = 0; i < 2; i++) {
             String str = Collections.max(list).toString();
